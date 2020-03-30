339f86711f148fbabbfda83197018c5e12e25b65
Amir Ghasemi <amirghasemi615@gmail.com>
2020-03-30 10:49:03 +0430

    .

--- E-Commerce/Areas/Admin/Controllers/CarController.cs
+++ E-Commerce/Areas/Admin/Controllers/CarController.cs
@@ -7,103 +7,109 @@ using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.Rendering;
 using Microsoft.EntityFrameworkCore;
 using System;
+using System.Linq;
+using System.Threading;
 using System.Threading.Tasks;
 
 namespace ECommerce.Areas.Admin.Controllers
 {
-	[Area("Admin")]
-	[Authorize(Roles = "Admin")]
-	public class CarController : Controller
-	{
-		private readonly ApplicationDbContext _context;
+    [Area("Admin")]
+    [Authorize(Roles = "Admin")]
+    public class CarController : Controller
+    {
+        private readonly ApplicationDbContext _context;
 
-		public CarController(ApplicationDbContext context)
-		{
-			_context = context;
-		}
+        public CarController(ApplicationDbContext context)
+        {
+            _context = context;
+        }
 
-		[AutoValidateAntiforgeryToken]
-		public async Task<IActionResult> Index()
-		{
-			return View(await _context.Cars.Include(x => x.Maker).ToListAsync());
-		}
+        [AutoValidateAntiforgeryToken]
+        public async Task<IActionResult> Index()
+        {
+            return View(await _context.Cars.Include(x => x.Maker).ToListAsync());
+        }
 
-		[HttpGet]
-		[AutoValidateAntiforgeryToken]
-		public async Task<IActionResult> AddEditCar(string id)
-		{
-			ViewBag.Makers = new SelectList(await _context.Makers.ToListAsync(), "Id", "Name");
+        [HttpGet]
+        [AutoValidateAntiforgeryToken]
+        public async Task<IActionResult> AddEditCar(string id)
+        {
+            ViewBag.Makers = new SelectList(await _context.Makers.ToListAsync(), "Id", "Name");
 
-			var car = await _context.Cars.FirstOrDefaultAsync(c => c.Id == id);
-			if (car != null)
-			{
-				return PartialView("AddEditCar", car);
-			}
+            var car = await _context.Cars.FirstOrDefaultAsync(c => c.Id == id);
 
-			return PartialView("AddEditCar", new Car());
-		}
+          
 
-		[HttpPost]
-		[ValidateAntiForgeryToken]
-		public async Task<IActionResult> AddEditCar(string id, Car model, string redirectUrl)
-		{
-			if (ModelState.IsValid)
-			{
-				if (String.IsNullOrWhiteSpace(id))
-				{
-					_context.Cars.Add(model);
-					await _context.SaveChangesAsync();
 
-					TempData["Notification"] = Notification.ShowNotif(MessageType.Add, ToastType.Green);
+            if (car != null)
+            {
+                return PartialView("AddEditCar", car);
+            }
 
-					return PartialView("_SuccessfulResponse", redirectUrl);
-				}
+            return PartialView("AddEditCar", new Car());
+        }
 
-				_context.Cars.Update(model);
-				await _context.SaveChangesAsync();
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> AddEditCar(string id, Car model, string redirectUrl)
+        {
+            if (ModelState.IsValid)
+            {
+                if (String.IsNullOrWhiteSpace(id))
+                {
+                    _context.Cars.Add(model);
+                    await _context.SaveChangesAsync();
 
-				TempData["Notification"] = Notification.ShowNotif(MessageType.Edit, ToastType.Blue);
+                    TempData["Notification"] = Notification.ShowNotif(MessageType.Add, ToastType.Green);
 
-				return PartialView("_SuccessfulResponse", redirectUrl);
-			}
+                    return PartialView("_SuccessfulResponse", redirectUrl);
+                }
 
-			ViewBag.Makers = new SelectList(await _context.Makers.ToListAsync(), "Id", "Name");
+                _context.Cars.Update(model);
+                await _context.SaveChangesAsync();
 
-			return PartialView("AddEditCar", model);
-		}
+                TempData["Notification"] = Notification.ShowNotif(MessageType.Edit, ToastType.Blue);
 
-		[HttpGet]
-		[AutoValidateAntiforgeryToken]
-		public async Task<IActionResult> DeleteCar(string id)
-		{
-			var car = await _context.Cars.FirstOrDefaultAsync(c => c.Id == id);
-			if (car == null)
-			{
-				return RedirectToAction("Index");
-			}
+                return PartialView("_SuccessfulResponse", redirectUrl);
+            }
 
-			return PartialView("DeleteCar", car.Name);
-		}
+            ViewBag.Makers = new SelectList(await _context.Makers.ToListAsync(), "Id", "Name");
 
-		[HttpPost]
-		[ValidateAntiForgeryToken]
-		public async Task<IActionResult> DeleteCar(string id, string redirectUrl)
-		{
-			if (ModelState.IsValid)
-			{
-				var model = await _context.Cars.FirstOrDefaultAsync(c => c.Id == id);
+            return PartialView("AddEditCar", model);
+        }
 
-				_context.Cars.Remove(model);
-				await _context.SaveChangesAsync();
+        [HttpGet]
+        [AutoValidateAntiforgeryToken]
+        public async Task<IActionResult> DeleteCar(string id)
+        {
+            var car = await _context.Cars.FirstOrDefaultAsync(c => c.Id == id);
+            if (car == null)
+            {
+                return RedirectToAction("Index");
+            }
 
-				TempData["Notification"] = Notification.ShowNotif(MessageType.Delete, ToastType.Red);
+            return PartialView("DeleteCar", car.Name);
+        }
 
-				return PartialView("_SuccessfulResponse", redirectUrl);
-			}
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> DeleteCar(string id, string redirectUrl)
+        {
+            if (ModelState.IsValid)
+            {
+                var model = await _context.Cars.FirstOrDefaultAsync(c => c.Id == id);
 
-			TempData["Notification"] = Notification.ShowNotif(MessageType.DeleteError, ToastType.Yellow);
+                _context.Cars.Remove(model);
+                await _context.SaveChangesAsync();
 
-			return RedirectToAction("Index");
-		}
-	}
+                TempData["Notification"] = Notification.ShowNotif(MessageType.Delete, ToastType.Red);
+
+                return PartialView("_SuccessfulResponse", redirectUrl);
+            }
+
+            TempData["Notification"] = Notification.ShowNotif(MessageType.DeleteError, ToastType.Yellow);
+
+            return RedirectToAction("Index");
+        }
+    }
 }
