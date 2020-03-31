744785788d6a677461945faffa2ad0981d569650
Amir Ghasemi <amirghasemi615@gmail.com>
2020-03-31 17:25:39 +0430

    Order Controller

---
+++ E-Commerce/Areas/Admin/Controllers/OrderController.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using ECommerce.Data;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+
+namespace E_Commerce.Areas.Admin.Controllers
+{
+    [Area("Admin")]
+    //[Authorize(Roles = "Admin")]
+    public class OrderController : Controller
+    {
+        private readonly ApplicationDbContext _context;
+
+        public OrderController(ApplicationDbContext context)
+        {
+            _context = context;
+        }
+
+        [HttpGet]
+        [AutoValidateAntiforgeryToken]
+        public async Task<IActionResult> Index()
+        {
+            return View(await _context.Orders.Include(o => o.Factor).Include(o => o.Status).ToListAsync());
+        }
+    }
+}
--- E-Commerce/Models/Order.cs
+++ E-Commerce/Models/Order.cs
@@ -1,40 +1,43 @@
 ﻿using System;
+using System.Collections.Generic;
 using System.ComponentModel.DataAnnotations;
 using System.ComponentModel.DataAnnotations.Schema;
 
 namespace ECommerce.Models
 {
-	public class Order
-	{
-		[Display(Name = "شناسه")]
-		[DatabaseGenerated(DatabaseGeneratedOption.Identity)]
-		public string Id { get; set; }
+    public class Order
+    {
+        [Display(Name = "شناسه")]
+        [DatabaseGenerated(DatabaseGeneratedOption.Identity)]
+        public string Id { get; set; }
 
-		[Display(Name = "فاکتور")]
-		public string FactorId { get; set; }
+        [Display(Name = "فاکتور")]
+        public string FactorId { get; set; }
 
-		[Display(Name = "فاکتور")]
-		public virtual Factor Factor { get; set; }
+        [Display(Name = "فاکتور")]
+        public virtual Factor Factor { get; set; }
 
-		[Display(Name = "شماره تراکنش")]
-		public string TransactionNumber { get; set; }
+        [Display(Name = "شماره تراکنش")]
+        public string TransactionNumber { get; set; }
 
-		[Display(Name = "وضعیت تراکنش")]
-		public bool TransactionStatus { get; set; }
+        [Display(Name = "وضعیت تراکنش")]
+        public bool TransactionStatus { get; set; }
 
-		[Display(Name = "تاریخ تراکنش")]
-		public DateTime TransactionDate { get; set; }
+        [Display(Name = "تاریخ تراکنش")]
+        public DateTime TransactionDate { get; set; }
 
-		[Display(Name = "وضعیت")]
-		public string StatusId { get; set; }
+        [Display(Name = "وضعیت")]
+        public string StatusId { get; set; }
 
-		[Display(Name = "وضعیت")]
-		public virtual Status Status { get; set; }
+        [Display(Name = "وضعیت")]
+        public virtual Status Status { get; set; }
 
-		[Display(Name = "کد درخواست")]
-		public long IssueCode { get; set; }
+        [Display(Name = "کد درخواست")]
+        public long IssueCode { get; set; }
 
-		[Display(Name = "نوضیحات")]
-		public string Description { get; set; }
-	}
+        [Display(Name = "نوضیحات")]
+        public string Description { get; set; }
+
+      
+    }
 }
