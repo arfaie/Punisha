70b6e83ffadfb8c4d52d13a63d3103bb82bc1326
Amir Ghasemi <amirghasemi615@gmail.com>
2020-03-31 12:02:15 +0430

    .

--- E-Commerce/Areas/Admin/Controllers/HistoryController.cs
+++ E-Commerce/Areas/Admin/Controllers/HistoryController.cs
@@ -3,12 +3,14 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
 using ECommerce.Data;
+using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.EntityFrameworkCore;
 
 namespace E_Commerce.Areas.Admin.Controllers
 {
     [Area("Admin")]
+    [Authorize(Roles = "Admin")]
     public class HistoryController : Controller
     {
         private readonly ApplicationDbContext _context;
