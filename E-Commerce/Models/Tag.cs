6d4d7dde2401f98997d6434f07bcd9920c3b9847
Amir Ghasemi <amirghasemi615@gmail.com>
2020-04-19 10:38:08 +0430

    News Tables

---
+++ E-Commerce/Models/Tag.cs
@@ -0,0 +1,16 @@
+﻿using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+
+namespace ECommerce.Models
+{
+    public class Tag
+    {
+        [Display(Name = "شناسه")]
+        [DatabaseGenerated(DatabaseGeneratedOption.Identity)]
+        public string Id { get; set; }
+
+        [Display(Name = "عنوان تگ")]
+        public string Title { get; set; }
+    }
+}
