b62f59d14a8586bd6eff91e29b6a33e6a0a3e2c8
Hamidnwo <baghernia@outlook.com>
2020-04-19 21:19:28 +0430

    Mobile Menu

---
+++ E-Commerce/ViewComponents/MobileMenuViewComponent.cs
@@ -0,0 +1,23 @@
+﻿using ECommerce.Data;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace ECommerce.ViewComponents
+{
+	public class MobileMenuViewComponent : ViewComponent
+	{
+		private readonly ApplicationDbContext _context;
+
+		public MobileMenuViewComponent(ApplicationDbContext context)
+		{
+			_context = context;
+		}
+
+		public async Task<IViewComponentResult> InvokeAsync()
+		{
+			return View(await _context.CategoryGroups.Include(x => x.Categories).OrderBy(x => x.Title).ToListAsync());
+		}
+	}
+}
