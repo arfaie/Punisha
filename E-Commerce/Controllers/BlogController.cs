fffdb6fd6f4e9dac47bf34782558f6a6e1cb2aff
arfaie <navid.arfaie@gmail.com>
2020-04-20 08:43:44 +0430

    Update Blog

---
+++ E-Commerce/Controllers/BlogController.cs
@@ -0,0 +1,45 @@
+﻿using System.Linq;
+using System.Threading.Tasks;
+using ECommerce.Data;
+using ECommerce.Models;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+
+namespace ECommerce.Controllers
+{
+    public class BlogController : Controller
+    {
+        private readonly ApplicationDbContext _context;
+        private readonly UserManager<ApplicationUser> _userManager;
+
+        public BlogController(ApplicationDbContext context, UserManager<ApplicationUser> userManager)
+        {
+            _context = context;
+            _userManager = userManager;
+        }
+
+        [HttpGet]
+        [AutoValidateAntiforgeryToken]
+        public async Task<IActionResult> Index()
+        {
+            ViewBag.path = "/upload/normalimage/";
+
+            ViewBag.tags = await _context.Tags.ToListAsync();
+
+            return View(await _context.Newses.Include(n => n.NewCategories).Include(x => x.NewsTagses).ThenInclude(x => x.tags).OrderByDescending(x => x.Id).ToListAsync());
+        }
+
+        [HttpGet]
+        [AutoValidateAntiforgeryToken]
+        public async Task<IActionResult> DetailesBlog(string id)
+        {
+            ViewBag.path = "/upload/normalimage/";
+
+            ViewBag.tags = await _context.Tags.ToListAsync();
+
+            return View(await _context.Newses.Include(n => n.NewCategories).Include(x => x.NewsTagses)
+                .ThenInclude(x => x.tags).FirstOrDefaultAsync(x => x.Id == id));
+        }
+    }
+}
