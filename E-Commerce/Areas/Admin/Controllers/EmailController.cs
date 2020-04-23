8b61ed03a56f0dc1c82a52b3aa58442b52f23e79
Amir Ghasemi <amirghasemi615@gmail.com>
2020-04-23 11:21:52 +0430

    Email Model

---
+++ E-Commerce/Areas/Admin/Controllers/EmailController.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using ECommerce.Data;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+
+namespace E_Commerce.Areas.Admin.Controllers
+{
+    [Area("Admin")]
+    public class EmailController : Controller
+    {
+        private readonly ApplicationDbContext _context;
+
+        public EmailController(ApplicationDbContext context)
+        {
+            _context = context;
+        }
+
+        [HttpGet]
+        [AutoValidateAntiforgeryToken]
+        public async Task<IActionResult> Index()
+        {
+            return View(await _context.Emails.ToListAsync());
+        }
+    }
+}
