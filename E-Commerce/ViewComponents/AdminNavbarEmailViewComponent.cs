34538d0526f3091fd37d9b9a983a0dd099f9e85d
Amir Ghasemi <amirghasemi615@gmail.com>
2020-04-25 19:37:44 +0430

    changes were made

---
+++ E-Commerce/ViewComponents/AdminNavbarEmailViewComponent.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using ECommerce.Data;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+
+namespace EShop.ViewComponents
+{
+    public class AdminNavbarEmailViewComponent : ViewComponent
+    {
+        private readonly ApplicationDbContext _context;
+
+        public AdminNavbarEmailViewComponent(ApplicationDbContext context)
+        {
+            _context = context;
+        }
+
+        public async Task<IViewComponentResult> InvokeAsync()
+        {
+            return View(await _context.Emails.Where(e => e.Readed == false).ToListAsync());
+        }
+
+    }
+}
---
+++ E-Commerce/ViewComponents/AdminNavbarProductViewComponent.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using ECommerce.Data;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+
+namespace EShop.ViewComponents
+{
+    public class AdminNavbarProductViewComponent : ViewComponent
+    {
+        private readonly ApplicationDbContext _context;
+
+        public AdminNavbarProductViewComponent(ApplicationDbContext context)
+        {
+            _context = context;
+        }
+
+        public async Task<IViewComponentResult> InvokeAsync()
+        {
+            ViewBag.rootpath = "/upload/thumbnailimage/";
+
+            return View(await _context.Products.Where(p => p.Inventory <= p.OrderPoint).ToListAsync());
+        }
+    }
+}
