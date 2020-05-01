23600c280c445ba413195b4c7b3a3a6c995ccd3f
Amir Ghasemi <amirghasemi615@gmail.com>
2020-05-01 11:38:18 +0430

    working on product

--- E-Commerce/Areas/Admin/Controllers/ProductGalleryController.cs
+++ E-Commerce/Areas/Admin/Controllers/ProductGalleryController.cs
@@ -14,157 +14,235 @@ using System.Collections.Generic;
 using System.IO;
 using System.Linq;
 using System.Threading.Tasks;
+using SessionExtensions = ECommerce.Helpers.SessionExtensions;
 
 namespace ECommerce.Areas.Admin.Controllers
 {
-	[Area("Admin")]
-	[Authorize(Roles = "Admin")]
-	public class ProductGalleryController : Controller
-	{
-		private readonly ApplicationDbContext _context;
+    [Area("Admin")]
+    [Authorize(Roles = "Admin")]
+    public class ProductGalleryController : Controller
+    {
+        private readonly ApplicationDbContext _context;
 
-		private readonly IWebHostEnvironment _env;
+        private readonly IWebHostEnvironment _env;
 
-		public ProductGalleryController(ApplicationDbContext context, IWebHostEnvironment env)
-		{
-			_context = context;
+        public ProductGalleryController(ApplicationDbContext context, IWebHostEnvironment env)
+        {
+            _context = context;
 
-			_env = env;
-		}
+            _env = env;
+        }
 
-		[AutoValidateAntiforgeryToken]
-		public async Task<IActionResult> Index(string id)
-		{
-			if (!String.IsNullOrWhiteSpace(id))
-			{
-				return RedirectToAction("Index", "Product");
-			}
+        [HttpGet]
+        [AutoValidateAntiforgeryToken]
+        public async Task<IActionResult> Index(string id)
+        {
+            if (String.IsNullOrWhiteSpace(id))
+            {
+                return RedirectToAction("Index", "Product");
+            }
 
-			return View(await _context.ProductGalleries.Where(g => g.ProductId == id).ToListAsync());
-		}
+            ViewBag.rootpath = "/upload/thumbnailimage/";
 
-		[HttpGet]
-		[AutoValidateAntiforgeryToken]
-		public async Task<IActionResult> AddEdit(string id)
-		{
-			ViewBag.Products = new SelectList(await _context.Products.ToListAsync(), "Id", "Name");
+            HttpContext.Session.SetString("IDProduct", id);
 
-			var productGallery = await _context.ProductGalleries.FirstOrDefaultAsync(c => c.Id == id);
-			if (productGallery != null)
-			{
-				return PartialView("AddEdit", productGallery);
-			}
+            var select = await _context.Products.FirstOrDefaultAsync(p => p.Id == id);
+            ViewBag.productname = select.Name;
 
-			return PartialView("AddEdit", new ProductGallery());
-		}
+            return View(await _context.ProductGalleries.Where(g => g.ProductId == id).ToListAsync());
+        }
 
-		[HttpPost]
-		[ValidateAntiForgeryToken]
-		public async Task<IActionResult> AddEdit(ProductGallery model, string id, IEnumerable<IFormFile> files)
-		{
-			if (ModelState.IsValid)
-			{
-				var stList = new List<string>();
+        [HttpGet]
+        [AutoValidateAntiforgeryToken]
+        public async Task<IActionResult> AddEdit(string id)
+        {
+            ViewBag.Products = new SelectList(await _context.Products.ToListAsync(), "Id", "Name");
 
-				var upload = Path.Combine(_env.WebRootPath.Replace("\\", "/") + Helper.NormalImagePath);
-				foreach (var file in files)
-				{
-					if (file != null && file.Length > 0)
-					{
-						var filename = Guid.NewGuid().ToString().Replace("-", "") + Path.GetExtension(file.FileName);
+            var productGallery = await _context.ProductGalleries.FirstOrDefaultAsync(c => c.Id == id);
+            if (productGallery != null)
+            {
+                return PartialView("AddEdit", productGallery);
+            }
 
-						await using (var fs = new FileStream(Path.Combine(upload, filename), FileMode.Create))
-						{
-							await file.CopyToAsync(fs);
-							stList.Add(filename);
-							//model.Image = filename;
-						}
+            return PartialView("AddEdit", new ProductGallery());
+        }
 
-						var img = new ImageResizer();
-						img.Resize(upload + filename, _env.WebRootPath + Helper.ThumbnailImagePath + filename);
-					}
-				}
-				if (model.Image == null)
-				{
-					model.Image = "defaultpic.png";
-				}
-				if (!String.IsNullOrWhiteSpace(id))
-				{
-					foreach (var variable in stList)
-					{
-						model.Image = variable;
-						//model.ProductId = (int)HttpContext.Session.GetInt32("ImageProductKey");
-						_context.ProductGalleries.Add(model);
-						_context.SaveChanges();
-					}
+        //[HttpPost]
+        //[ValidateAntiForgeryToken]
+        //public async Task<IActionResult> AddEdit(ProductGallery model, string id, IEnumerable<IFormFile> files, string imgName)
+        //{
+        //    if (ModelState.IsValid)
+        //    {
+        //        var stList = new List<string>();
 
-					return Json(new { Status = "success", Message = "عکس با موفقیت ثبت شد" });
-				}
+        //        var upload = Path.Combine(_env.WebRootPath.Replace("\\", "/") + Helper.NormalImagePath);
+        //        foreach (var file in files)
+        //        {
+        //            if (file != null && file.Length > 0)
+        //            {
+        //                var filename = Guid.NewGuid().ToString().Replace("-", "") + Path.GetExtension(file.FileName);
 
-				foreach (var variable in stList)
-				{
-					model.Image = variable;
+        //                await using (var fs = new FileStream(Path.Combine(upload, filename), FileMode.Create))
+        //                {
+        //                    await file.CopyToAsync(fs);
+        //                    stList.Add(filename);
+        //                    //model.Image = filename;
+        //                }
 
-					_context.ProductGalleries.Update(model);
-					_context.SaveChanges();
-				}
+        //                var img = new ImageResizer();
+        //                img.Resize(upload + filename, _env.WebRootPath + Helper.ThumbnailImagePath + filename);
+        //            }
+        //        }
+        //        if (model.Image == null)
+        //        {
+        //            model.Image = "defaultpic.png";
+        //        }
+        //        if (!String.IsNullOrWhiteSpace(id))
+        //        {
+        //            foreach (var variable in stList)
+        //            {
+        //                model.Image = variable;
+        //                model.ProductId = HttpContext.Session.GetString("IDProduct");
+        //                //model.ProductId = (int)HttpContext.Session.GetInt32("ImageProductKey");
+        //                _context.ProductGalleries.Add(model);
+        //                _context.SaveChanges();
+        //            }
 
-				return Json(new { Status = "success", Message = "عکس با موفقیت ویرایش شد" });
-			}
+        //            return Json(new { Status = "success", Message = "عکس با موفقیت ثبت شد" });
+        //        }
 
-			ViewBag.Products = new SelectList(await _context.Products.ToListAsync(), "Id", "Name");
+        //        foreach (var variable in stList)
+        //        {
+        //            model.Image = variable;
 
-			var list = new List<string>();
-			foreach (var validation in ViewData.ModelState.Values)
-			{
-				list.AddRange(validation.Errors.Select(error => error.ErrorMessage));
-			}
+        //            _context.ProductGalleries.Update(model);
+        //            _context.SaveChanges();
+        //        }
 
-			return Json(new { Status = "error", error = list });
-		}
+        //        return Json(new { Status = "success", Message = "عکس با موفقیت ویرایش شد" });
+        //    }
 
-		[HttpGet]
-		[AutoValidateAntiforgeryToken]
-		public async Task<IActionResult> Delete(string id)
-		{
-			var productGallery = await _context.ProductGalleries.FirstOrDefaultAsync(c => c.Id == id);
-			if (productGallery == null)
-			{
-				return RedirectToAction("Index");
-			}
+        //    ViewBag.Products = new SelectList(await _context.Products.ToListAsync(), "Id", "Name");
 
-			return PartialView("Delete", productGallery.Image);
-		}
+        //    var list = new List<string>();
+        //    foreach (var validation in ViewData.ModelState.Values)
+        //    {
+        //        list.AddRange(validation.Errors.Select(error => error.ErrorMessage));
+        //    }
 
-		[HttpPost]
-		[ValidateAntiForgeryToken]
-		public async Task<IActionResult> Delete(string id, string redirectUrl)
-		{
-			if (ModelState.IsValid)
-			{
-				var model = await _context.ProductGalleries.SingleOrDefaultAsync(b => b.Id == id);
+        //    return Json(new { Status = "error", error = list });
+        //}
 
-				var sourcePath = Path.Combine(_env.WebRootPath.Replace("\\", "/") + Helper.NormalImagePath, model.Image);
-				if (System.IO.File.Exists(sourcePath))
-				{
-					System.IO.File.Delete(sourcePath);
-				}
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> AddEdit(ProductGallery model, string imgName, string id, IEnumerable<IFormFile> files)
+        {
+            if (ModelState.IsValid)
+            {
+                //upload image
+                var uploads = Path.Combine(_env.WebRootPath, "upload\\normalimage\\");
 
-				var sourcePath2 = Path.Combine(_env.WebRootPath.Replace("\\", "/") + Helper.ThumbnailImagePath, model.Image);
-				if (System.IO.File.Exists(sourcePath2))
-				{
-					System.IO.File.Delete(sourcePath2);
-				}
+                foreach (var file in files)
+                {
+                    if (file != null && file.Length > 0)
+                    {
+                        var filename = Guid.NewGuid().ToString().Replace("-", "") + Path.GetExtension(file.FileName);
 
-				_context.ProductGalleries.Remove(model);
-				await _context.SaveChangesAsync();
+                        await using (var fs = new FileStream(Path.Combine(uploads, filename), FileMode.Create))
+                        {
+                            await file.CopyToAsync(fs);
+                            model.Image = filename;
+                        }
+                        var img = new ImageResizer();
+                        img.Resize(uploads + filename, _env.WebRootPath + "\\upload\\thumbnailimage\\" + filename);
+                    }
+                }
+                //upload image
 
-				TempData["Notification"] = Notification.ShowNotif(MessageType.Delete, ToastType.Red);
-				return PartialView("_SuccessfulResponse", redirectUrl);
-			}
+               
+                if (String.IsNullOrWhiteSpace(id))
+                {
+                    if (model.Image == null)
+                    {
+                        model.Image = "defaultpic.png";
+                    }
 
-			TempData["Notification"] = Notification.ShowNotif(MessageType.DeleteError, ToastType.Red);
-			return RedirectToAction("Index");
-		}
-	}
+                    model.ProductId= HttpContext.Session.GetString("IDProduct");
+
+                    _context.ProductGalleries.Add(model);
+                    await _context.SaveChangesAsync();
+
+                    TempData["Notification"] = Notification.ShowNotif(MessageType.Add, ToastType.Green);
+                    return RedirectToAction("Index");
+                }
+
+                if (model.Image == null)
+                {
+                    model.Image = imgName;
+                }
+
+                _context.ProductGalleries.Update(model);
+                await _context.SaveChangesAsync();
+
+                TempData["Notification"] = Notification.ShowNotif(MessageType.Edit, ToastType.Blue);
+                return RedirectToAction("Index");
+            }
+            if (!String.IsNullOrWhiteSpace(id))
+            {
+                TempData["Notification"] = Notification.ShowNotif(MessageType.AddError, ToastType.Yellow);
+            }
+            else
+            {
+                TempData["Notification"] = Notification.ShowNotif(MessageType.EditError, ToastType.Yellow);
+            }
+
+            ViewBag.Products = new SelectList(await _context.Products.ToListAsync(), "Id", "Name");
+
+            return PartialView("AddEdit", model);
+        }
+
+        [HttpGet]
+        [AutoValidateAntiforgeryToken]
+        public async Task<IActionResult> Delete(string id)
+        {
+            var productGallery = await _context.ProductGalleries.FirstOrDefaultAsync(c => c.Id == id);
+            if (productGallery == null)
+            {
+                return RedirectToAction("Index");
+            }
+
+            return PartialView("Delete", productGallery.Image);
+        }
+
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> Delete(string id, string redirectUrl)
+        {
+            if (ModelState.IsValid)
+            {
+                var model = await _context.ProductGalleries.SingleOrDefaultAsync(b => b.Id == id);
+
+                var sourcePath = Path.Combine(_env.WebRootPath.Replace("\\", "/") + Helper.NormalImagePath, model.Image);
+                if (System.IO.File.Exists(sourcePath))
+                {
+                    System.IO.File.Delete(sourcePath);
+                }
+
+                var sourcePath2 = Path.Combine(_env.WebRootPath.Replace("\\", "/") + Helper.ThumbnailImagePath, model.Image);
+                if (System.IO.File.Exists(sourcePath2))
+                {
+                    System.IO.File.Delete(sourcePath2);
+                }
+
+                _context.ProductGalleries.Remove(model);
+                await _context.SaveChangesAsync();
+
+                TempData["Notification"] = Notification.ShowNotif(MessageType.Delete, ToastType.Red);
+                return PartialView("_SuccessfulResponse", redirectUrl);
+            }
+
+            TempData["Notification"] = Notification.ShowNotif(MessageType.DeleteError, ToastType.Red);
+            return RedirectToAction("Index");
+        }
+    }
 }
---
+++ E-Commerce/ViewComponents/AdminNavbarOrdersViewComponent.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using ECommerce.Data;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+
+namespace EShop.ViewComponents
+{
+    public class AdminNavbarOrdersViewComponent : ViewComponent
+    {
+        private readonly ApplicationDbContext _context;
+
+        public AdminNavbarOrdersViewComponent(ApplicationDbContext context)
+        {
+            _context = context;
+        }
+
+        public async Task<IViewComponentResult> InvokeAsync()
+        {
+            return View(await _context.Orders.Where(e => e.StatusId == "6f9c65d681937c32dafcec03").ToListAsync());
+        }
+    }
+}
+
